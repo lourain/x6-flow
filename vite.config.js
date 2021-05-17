378d8e39877f1e60c89951a60f8f1c57145123bf
lourain <wsxrk007@163.com>
2021-05-17 21:23:38 +0800

    doc

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import styleImport from "vite-plugin-style-import"
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir:'docs',
-    assetsDir: 'x6-flow/assets'
+    outDir: 'docs',
+    assetsDir: './assets'
   },
   plugins: [
     vue(),
